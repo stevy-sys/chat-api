1cfcccbf04f1e61ad6c23e96e13510ef9147e143
stevy <stevyralambomanana@gmail.com>
2023-07-08 14:23:52 +0200

    auth reglage

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,38 +3,48 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Service\AuthService;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\AuthRequest;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
-    {
-        // Validation des informations de connexion
-        $credentials = [
-            'email' => $request->input('email'),
-            'password' => $request->input('password'),
-        ];
+    public $authService ;
 
+    public function __construct(){
+        $this->authService = new AuthService();
+    }
+
+    public function login(Request $request){
         $email = User::where('email',$request->input('email'))->first();
         $error = [] ;
         if (!isset($email)) {
             $error[] = "email n'existe pas";
             return $this->sendError('erreur email',$error,401);
         }
-        if (Auth::attempt($credentials)) {
+    
+        if (Auth::attempt($request->all())) {
             $user = Auth::user();
-            $token = $user->createToken('token-name')->plainTextToken;
-
-            return $this->sendResponse(compact('user','token'),'user connected');
+            return  $this->finalResponse($user);
         }
         $error[] = "mot de passe invalide";
         return $this->sendError('erreur password',$error,401);
     }
 
     public function register(Request $request) {
-        
+        $error = $this->authService->validateRegister($request);
+        if (count($error) > 0) {
+            return $this->sendError('erreur inscription',$error,401);
+        }
+        $user = $this->authService->createUser($request->name,$request->email,$request->password);
+        return  $this->finalResponse($user);
+    }
+
+    private function finalResponse($user) {
+        $token = $this->authService->createToken($user);
+        return $this->sendResponse(compact('user','token'),'user connected'); 
     }
 
     public function notAuth() {
---
+++ app/Service/AuthService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Service;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthService 
+{
+   
+
+    public function validateRegister($request) : Array
+    {
+        $error = [] ;
+        if (!isset($request->email)) {
+            $error['email'][] = 'email invalide ';
+        }
+        $user = User::where('email',$request->email)->first();
+        if (isset($user)) {
+            $error['email'][] = 'email deja pris ';
+        }
+        if (!isset($request->password)) {
+            $error['password'][] = 'password invalide ';
+        }
+        if (!isset($request->name)) {
+            $error['name'][] = 'name invalide ';
+        }
+        if (!isset($request->confirm_password)) {
+            $error['confirm_password'][] = 'confirm_password invalide ';
+        }
+        if (isset($request->confirm_password) && isset($request->password) && ($request->confirm_password !== $request->password)) {
+            $error['error'][] = 'password not match ';
+        }
+        return $error ; 
+    }
+
+    
+    public function createToken($user) : String {
+        $token = $user->createToken('token-name')->plainTextToken;
+        return $token ;
+    }
+
+    public function createUser($name,$email,$password) : User {
+        $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password) ,
+        ]);
+        return $user ;
+    }
+}
