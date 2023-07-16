4a0aab3ac4d9b423e82a24641541d0b4c3955035
stevy <stevyralambomanana@gmail.com>
2023-07-16 23:24:06 +0200

    test

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://chat-vue-nine.vercel.app'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
