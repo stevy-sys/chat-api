9cd72c3c2647b2fbdad0026116730c1765bc8342
stevy <stevyralambomanana@gmail.com>
2023-07-18 14:43:16 +0200

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,8 +16,12 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)
-                ->header('Access-Control-Allow-Origin', '*')
-                ->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        $response = $next($request);
+
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+
+        return $response;
     }
 }
