a2875052873a809ff5a5d5c3cec4d1557edabde1
stevy <stevyralambomanana@gmail.com>
2023-07-18 13:44:54 +0200

    cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie','api'],
+    'paths' => ['https://dev.rubycorp.fr/api/connexion', 'sanctum/csrf-cookie','api'],
 
     'allowed_methods' => ['*'],
 
