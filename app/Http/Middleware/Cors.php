8630e422a886c2a96f27eb8415382879f1d6b61f
stevy <stevyralambomanana@gmail.com>
2023-07-28 19:23:21 +0200

    test

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response as FacadesResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class Cors
@@ -15,7 +16,7 @@ class Cors
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Response $request, Closure $next)
     {
         $response = $next($request);
 
