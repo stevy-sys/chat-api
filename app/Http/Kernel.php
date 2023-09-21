16ae3c8b26f012267499979e351cbd4d3b1b6dd7
stevy <stevyralambomanana@gmail.com>
2023-09-21 18:06:21 +0200

    active cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        // \Illuminate\Http\Middleware\HandleCors::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         // \App\Http\Middleware\Cors::class,
